9149f795bee5b5ead0b86fad67ee1c0a0339ff8f
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-07 21:46:43 +0700

    feat(components): Request for comments and output

--- client/src/components/Comments/Comments.tsx
+++ client/src/components/Comments/Comments.tsx
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux';
+import CommentsInput from '../CommentsInput/CommentsInput';
 import './Comments.scss';
 
 const Comments = () => {
+  const comments = useSelector((state: { comments: any }) => state.comments);
 
   return (
     <div className="comments">
       <ul className="comments__list">
+        {comments.comments.map((res: { text: string; id: number }) => (
+          <li key={res.id}>{res.text}</li>
+        ))}
       </ul>
+      <CommentsInput />
     </div>
   );
 };
