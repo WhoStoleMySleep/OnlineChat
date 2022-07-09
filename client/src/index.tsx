af40f9c82987ee14fd832e4088ded4f2abbdee73
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-09 19:19:44 +0700

    feat: initializing 'apollo'

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,26 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { graphql } from '@apollo/react-hoc';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store';
 import App from './App';
 import './index.scss';
+import store from './redux/store';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:3005/graphql',
+});
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
