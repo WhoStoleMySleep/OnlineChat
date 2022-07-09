a35754885ecd83d492c4b947a744049c91335543
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-09 19:15:33 +0700

    feat: adding 'cors'

--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
@@ -9,6 +10,8 @@ const PORT = 3005;
 mongoose.connect(
   'mongodb+srv://konstantin:e3oNRt6py371SPuh@onlinechat.nxh452d.mongodb.net/?retryWrites=true&w=majority'
 );
+
+app.use(cors());
 
 app.use(
   '/graphql',
