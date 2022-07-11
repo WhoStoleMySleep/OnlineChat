de163921dba14815575926559a7a0b8f8d2c39fd
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-11 23:50:33 +0700

    feat: wrote the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,55 @@
+const express = require('express');
+const { createServer } = require('http');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const { execute, subscribe } = require('graphql');
+const { ApolloServer } = require('apollo-server-express');
+const mongoose = require('mongoose');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
+const MONGODB =
+  'mongodb+srv://konstantin:e3oNRt6py371SPuh@onlinechat.nxh452d.mongodb.net/?retryWrites=true&w=majority';
+
+(async () => {
+  const app = express();
+
+  const httpServer = createServer(app);
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const subscriptionServer = SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: '/graphql' }
+  );
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+    ],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  mongoose.connect(MONGODB, { useNewUrlParser: true });
+
+  const PORT = 3005;
+
+  httpServer.listen(
+    PORT,
+    console.log(`Http server is now running on port ${PORT}`)
+  );
+})();
