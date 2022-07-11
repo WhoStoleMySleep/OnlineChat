640dca0204e8e5debc91ef3f8298189e6a5f8f1f
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-11 23:43:06 +0700

    feat: rewrote the resolvers

---
+++ server/graphql/resolvers.js
@@ -0,0 +1,36 @@
+const Message = require('../models/Message');
+const { PubSub } = require('graphql-subscriptions');
+
+const pubsub = new PubSub();
+
+module.exports = {
+  Mutation: {
+    async createMessage(_, { messageInput: { text, author } }) {
+      const newMessage = new Message({
+        text: text,
+        author: author,
+      });
+
+      const res = await newMessage.save();
+
+      pubsub.publish('MESSAGE_CREATED', {
+        messageCreated: {
+          text: text,
+          author: author,
+        },
+      });
+
+      return {
+        id: res.id,
+        ...res._doc,
+      };
+    },
+  },
+  Subscription: {
+    messageCreated: {
+      subscribe: () => pubsub.asyncIterator('MESSAGE_CREATED'),
+    },
+  },
+  Query: {
+  },
+};
