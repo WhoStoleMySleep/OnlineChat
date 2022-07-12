79f4d7c102c0dd0dabe2f085cd30037f39855e11
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-12 23:34:10 +0700

    feat(components): data extraction

--- client/src/components/CommentsList/CommentsList.tsx
+++ client/src/components/CommentsList/CommentsList.tsx
@@ -25,10 +25,29 @@ const MESSAGES_QUERY = gql`
 
 const CommentsList = () => {
   const comments = useSelector((state: { comments: any }) => state.comments);
+  const dispatch = useDispatch();
+
+  const idGen = () => {
+    const min = Math.ceil(0);
+    const max = Math.floor(999999999);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
   const { loading: loadingQuery, data: dataQuery } = useQuery(MESSAGES_QUERY);
+
   useEffect(() => {
     if (!loadingQuery) dispatch(setComments(dataQuery.messages));
   }, [!loadingQuery]);
+
+  const { loading, data } = useSubscription(MESSAGES_SUBSCRIPTION, {
+    onSubscriptionData: (data) => {
+      const message = data.subscriptionData.data.messageCreated;
+
+      dispatch(
+        setComments([...comments.comments, { id: idGen(), ...message }])
+      );
+    },
+  });
 
   return (
     <ul className="comments-list">
