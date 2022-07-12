707bcaa572e98e13685c4ce036d5196201997ae8
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-12 23:25:25 +0700

    feat(client): connecting to a server

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,45 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { graphql } from '@apollo/react-hoc';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.scss';
 import store from './redux/store';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
+  uri: 'https://adasdasdasdasdasdsdfsdfsdfsdfs.herokuapp.com/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://adasdasdasdasdasdsdfsdfsdfsdfs.herokuapp.com/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
-  uri: 'http://localhost:3005/graphql',
 });
 
 const container = document.getElementById('root')!;
