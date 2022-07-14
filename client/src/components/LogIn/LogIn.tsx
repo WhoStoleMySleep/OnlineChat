de3894959dbd470e065448fe84f7d30b3dc48824
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-14 16:03:55 +0700

    feat(components): add author, send to localStorage

--- client/src/components/LogIn/LogIn.tsx
+++ client/src/components/LogIn/LogIn.tsx
@@ -1,35 +1,63 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAuthor } from '../../redux/componentReducers/login';
 import './LogIn.scss';
 
 const LogIn = () => {
-  const onFromHandle = () => {
+  const getLocalStorageAuthor = localStorage.getItem('author');
+
+  const [authorState, setAuthorState] = useState(
+    getLocalStorageAuthor ? getLocalStorageAuthor : ''
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setAuthor(authorState));
+  }, []);
+
+  const onFromOpen = () => {
     const form = document.querySelector('.log-in__form');
 
     if (form) {
-      form.classList.toggle('open');
+      form.classList.add('open');
     }
+  };
+
+  const onFormSubmit = (event?: any) => {
+    event?.preventDefault();
+    const form = document.querySelector('.log-in__form');
+
+    if (form && authorState) {
+      form?.classList.remove('open');
+      localStorage.setItem('author', authorState);
+      dispatch(setAuthor(authorState));
+    }
+  };
+
+  const onInputChange = (event: any) => {
+    setAuthorState(event.target.value);
   };
 
   return (
     <div className="log-in">
-      <button
-        type="button"
-        className="log-in__open-form"
-        onClick={onFromHandle}
-      >
+      <button type="button" className="log-in__open-form" onClick={onFromOpen}>
         Log-In
       </button>
-      <form action="" className="log-in__form">
+      <form action="" className="log-in__form" onSubmit={onFormSubmit}>
         <input
           type="text"
           className="log-in__author"
           placeholder="Enter your nickname"
           maxLength={25}
+          value={authorState}
+          onChange={(event) => onInputChange(event)}
         />
         <br />
         <button
           type="button"
           className="log-in__close-form"
-          onClick={onFromHandle}
+          onClick={onFormSubmit}
         >
           Сonfirm
         </button>
