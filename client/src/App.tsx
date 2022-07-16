3cb72590bc5474a2b0797a4acabccd982b1840fe
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-16 21:02:27 +0700

    feat(client): title when filling 'UnreadMessages'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,29 @@
 import './App.scss';
 import Messages from './components/Messages/Messages';
 import LogIn from './components/LogIn/LogIn';
+import { useEffect } from 'react';
 import PopUp from './components/PopUp/PopUp';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { unreadMessages } = useSelector(
+    (state: { unreadMessages: { unreadMessages: string[] } }) =>
+      state.unreadMessages
+  );
+
+  useEffect(() => {
+    if (unreadMessages.length) {
+      document.title = 'New messages!';
+    } else {
+      document.title = 'OnlineChat';
+    }
+  }, [unreadMessages.length]);
+
   return (
     <div className="App">
       <LogIn />
       <Messages />
+      <PopUp headed={'New messages!'} />
     </div>
   );
 }
