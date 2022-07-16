15dbc292081503a9217a9b5cc02fe544e39f2d0f
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-07-16 20:56:14 +0700

    feat(redux): create a 'unreadMessages' reducer

---
+++ client/src/redux/componentReducers/unreadMessages.ts
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const unreadMessages = createSlice({
+  name: 'unreadMessages',
+  initialState: {
+    unreadMessages: []
+  },
+  reducers: {
+    setUnreadMessages: (state, action) => {
+      state.unreadMessages = action.payload;
+    }
+  }
+});
+
+export const { setUnreadMessages } = unreadMessages.actions;
+
+export default unreadMessages.reducer;
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import author from './componentReducers/login';
 import messages from './componentReducers/messages';
+import unreadMessages from './componentReducers/unreadMessages';
 
 export default configureStore({
   reducer: {
     messages,
-    author
+    author,
+    unreadMessages
   }
 }); 
