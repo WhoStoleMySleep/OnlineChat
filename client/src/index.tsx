517dacaef16a6b00cf2047b6f2add0bf9592eae9
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-08-19 18:30:36 +0700

    refactor(client): Move the index.scss to 'styles'

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,7 +11,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.scss';
+import './styles/index.scss';
 import store from './redux/store';
 
 import './index.d';
