a369496e565313fd960d3ebd24864d84266443e0
WhoStoleMySleep <98680950+WhoStoleMySleep@users.noreply.github.com>
2022-08-20 12:53:10 +0700

    refactor(client): Make scss a module (Messages)

--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import MessagesInput from '../MessagesInput/MessagesInput';
 import MessagesList from '../MessagesList/MessagesList';
-import './Messages.scss';
+import styles from './Messages.module.scss';
 
 function Messages() {
   return (
-    <div className="messages">
+    <div className={styles.messages}>
       <MessagesList />
       <MessagesInput />
     </div>
